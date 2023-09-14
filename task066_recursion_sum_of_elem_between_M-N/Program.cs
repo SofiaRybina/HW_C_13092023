e2c2fb43608dd784ba5a58650e87c58247c61f0e
Sofia <rybina.sofia@gmail.com>
2023-09-14 08:34:23 +0300

    Рекурсия. Сумма натуральных элементов от M до N

---
+++ task066_recursion_sum_of_elem_between_M-N/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write($"Введите число M -> ");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("");
+
+Console.Write($"Введите число N -> ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine("");
+
+Console.WriteLine($"Cуммa натуральных элементов в промежутке от {M} до {N} -> {SumOfNaturalNumbers(M, N)}");
+
+int SumOfNaturalNumbers(int a, int b)
+{
+    if (a == 0)
+    {
+        return (b * (b + 1)) / 2;
+    }
+    else if (b == 0)
+    {
+        return (a * (a + 1)) / 2;
+    }
+    else if (a == b)
+    {
+        return a;
+    }
+    else if (a < b)
+    {
+        return b + SumOfNaturalNumbers(a, b - 1);
+    }
+    else
+        return b + SumOfNaturalNumbers(a, b + 1);
+}
